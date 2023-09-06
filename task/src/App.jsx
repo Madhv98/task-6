2749fa3e5e231b382018fb5892f0c7cf15badfc4
Madhav Dhital <mdhital@madhavs-mbp.mynetworksettings.com>
2023-09-05 23:10:28 -0400

    added simple page components

---
+++ task/src/App.jsx
@@ -0,0 +1,16 @@
+// Create App component
+
+import Footer from './footer/footer'
+import Header from './header/header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>Hello App</div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
